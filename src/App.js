f46958198d7ae6b35eecb0a16db740ab55692229
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2020-02-11 01:25:20 -0800

    Finished Boggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestInput from './testInput';
+import BoggleGrid from './components/gridFile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        function TextInput(){
+            return(
+              <button>
+                Button
+              </button>
+            );
+            
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          }
+          <BoggleGrid container > 
+        </BoggleGrid>
         </p>
         <a
           className="App-link"
---
+++ src/testInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+export default TestInput
+
+//const[text,setText] = useState("");
+
+function TestInput() {
+    
+    return (
+    <button>
+        Hello
+    </button>
+    );
+    
+
+}
+
+
